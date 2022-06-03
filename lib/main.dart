5c2fda64d47d3270c5a6d4a789dd5e27baa75d7b
Michael Norris <imichaelnorris@gmail.com>
2022-06-03 14:05:57 -0700

    add url strategy

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 import 'birdpress.dart';
 
 void main() {
+  // Needed to not have the default path be /#/
+  setPathUrlStrategy();
   runApp(const BirdPress());
 }
